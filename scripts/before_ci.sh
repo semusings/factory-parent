1ef82cba3aa808f98e2c278a5fbc94a4dca7a01d
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-04-03 09:50:52 +0545

    wip

--- scripts/before_ci.sh
+++ scripts/before_ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Using xmllint is faster than invoking maven
+#Using cat is faster than invoking maven
 ARTIFACT_VERSION="$(cat pom.xml | grep '<version>' | head -1 | sed 's/ //g' | sed 's/<version>//g' | sed 's/<\/version>//g' | sed 's/\s//g')"
 IS_RELEASE=$([ "${ARTIFACT_VERSION/SNAPSHOT}" == "${ARTIFACT_VERSION}" ] && echo 'true')
 export ARTIFACT_VERSION
